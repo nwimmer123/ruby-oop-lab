0aad759391a27f7a77f26f657f7d003675f998e9
Noah Wimmer <nwimmer123@yahoo.com>
2016-01-06 12:23:19 -0800

    Finally finished

--- lib/race.rb
+++ lib/race.rb
@@ -2,6 +2,33 @@ require_relative "car"
 
 class Race
 
-  # write Race class code here
+
+	def initialize
+		@car1 = Car.new
+		@car1.accelerate(rand(101))
+		@car2 = Car.new
+		@car2.accelerate(rand(101))
+	end
+
+	def cars
+		[@car1, @car2]
+	end
+
+	def winner
+		if @car1.speed > @car2.speed
+			@car1
+		else
+			@car2
+		end
+	end
+
+	def loser
+		if @car1.speed < @car2.speed
+			@car1
+		else
+			@car2
+		end
+		
+	end
 
 end
